2b8ea2f4b09d0a53667cde3a411890d5381c454a
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-11-20 13:14:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
@@ -16,7 +15,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Authentication helpers
 export async function signUp(email, password) {
   return createUserWithEmailAndPassword(auth, email, password);
 }
